51002485267de1ca237b41af8038485c93adb1eb
muammerhallac <muammerhallac@gmail.com>
2019-08-14 23:53:23 +0300

    Homepage tests finished

--- StoreFront.Tests/HomePageTests.cs
+++ StoreFront.Tests/HomePageTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace StoreFront.Tests
 {
@@ -7,31 +9,33 @@ namespace StoreFront.Tests
     public class HomePageTests
     {
         #region Fields
-        IWebDriver chromeDriver;
+        IWebDriver driver;
         const string HomePageUrl = "https://www.hangikredi.com";
+        const string DriversBase = "Drivers";
         #endregion
 
         [TestInitialize]
         public void Initialize()
         {
-            chromeDriver = new OpenQA.Selenium.Chrome.ChromeDriver("Drivers");
+            //driver = new OpenQA.Selenium.Firefox.FirefoxDriver(DriversBase);
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver(DriversBase);
         }
-
+        
         [TestMethod]
         public void Can_Navigate_To_Main_Page()
         {
-            chromeDriver.Navigate().GoToUrl(HomePageUrl);
-            IWebElement logo = chromeDriver.FindElement(By.ClassName("home-logo"));
+            driver.Navigate().GoToUrl(HomePageUrl);
+            IWebElement logo = driver.FindElement(By.ClassName("home-logo"));
             Assert.IsNotNull(logo);
         }
 
         [TestMethod]
         public void Can_Display_Credit_Tab()
         {
-            chromeDriver.Navigate().GoToUrl(HomePageUrl);
-            var creditTab = chromeDriver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[0];
+            driver.Navigate().GoToUrl(HomePageUrl);
+            var creditTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[0];
             creditTab.Click();
-            var showBidsButton = chromeDriver.FindElement(By.Id("teklif-goster"));
+            var showBidsButton = driver.FindElement(By.Id("teklif-goster"));
 
             Assert.IsNotNull(creditTab);
             Assert.IsNotNull(showBidsButton);
@@ -39,16 +43,30 @@ namespace StoreFront.Tests
         }
 
         [TestMethod]
+        public void Can_Display_Credit_Card_Tab()
+        {
+            driver.Navigate().GoToUrl(HomePageUrl);
+
+            var creditCardTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[1];
+            creditCardTab.Click();
+
+            var creditCardPageListingButton = driver.FindElement(By.Id("listButton"));
+
+            Assert.IsNotNull(creditCardPageListingButton);
+            Assert.IsTrue(creditCardPageListingButton.Displayed);
+        }
+
+        [TestMethod]
         public void Can_Display_Insurance_Tab()
         {
-            chromeDriver.Navigate().GoToUrl(HomePageUrl);
+            driver.Navigate().GoToUrl(HomePageUrl);
 
-            var insuranceTab = chromeDriver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[2];
+            var insuranceTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[2];
             insuranceTab.Click();
 
-            var insuranceRedirectButton = chromeDriver.FindElement(By.LinkText("Kasko Teklifleri"));
-            var trafficInsuranceRedirectButton = chromeDriver.FindElement(By.LinkText("Trafik Sigortası Teklifleri"));
-            var healthInsuranceRedirectButton = chromeDriver.FindElement(By.LinkText("Sağlık Sigortası Teklifleri"));
+            var insuranceRedirectButton = driver.FindElement(By.LinkText("Kasko Teklifleri"));
+            var trafficInsuranceRedirectButton = driver.FindElement(By.LinkText("Trafik Sigortası Teklifleri"));
+            var healthInsuranceRedirectButton = driver.FindElement(By.LinkText("Sağlık Sigortası Teklifleri"));
 
             Assert.IsNotNull(insuranceTab);
             Assert.IsNotNull(insuranceRedirectButton);
@@ -63,14 +81,14 @@ namespace StoreFront.Tests
         [TestMethod]
         public void Can_Display_Deposit_Tab()
         {
-            chromeDriver.Navigate().GoToUrl(HomePageUrl);
+            driver.Navigate().GoToUrl(HomePageUrl);
 
-            var depositTab = chromeDriver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[3];
+            var depositTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[3];
             depositTab.Click();
 
-            var depositCalculationRedirectButton = chromeDriver.FindElement(By.LinkText("Mevduat Hesaplama"));
-            var currencyRedirectButton = chromeDriver.FindElement(By.LinkText("Döviz"));
-            var goldRedirectButton = chromeDriver.FindElement(By.LinkText("Altın"));
+            var depositCalculationRedirectButton = driver.FindElement(By.LinkText("Mevduat Hesaplama"));
+            var currencyRedirectButton = driver.FindElement(By.LinkText("Döviz"));
+            var goldRedirectButton = driver.FindElement(By.LinkText("Altın"));
 
             Assert.IsNotNull(depositTab);
             Assert.IsNotNull(depositCalculationRedirectButton);
@@ -85,16 +103,16 @@ namespace StoreFront.Tests
         [TestMethod]
         public void Can_Display_Internet_Tab()
         {
-            chromeDriver.Navigate().GoToUrl(HomePageUrl);
+            driver.Navigate().GoToUrl(HomePageUrl);
 
-            var internetTab = chromeDriver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[4];
+            var internetTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[4];
             internetTab.Click();
 
-            var leanInternetRedirectButton = chromeDriver.FindElement(By.LinkText("Yalın İnternet"));
-            var phonePlusInternetRedirectButton = chromeDriver.FindElement(By.LinkText("Telefon + İnternet"));
-            var tvPlusInternetRedirectButton = chromeDriver.FindElement(By.LinkText("Tv + İnternet"));
-            var triplePackageRedirectButton = chromeDriver.FindElement(By.LinkText("3’lü Paket"));
-            var allInternetPackagesRedirectButton = chromeDriver.FindElement(By.LinkText("Tüm İnternet Paketlerini Gör"));
+            var leanInternetRedirectButton = driver.FindElement(By.LinkText("Yalın İnternet"));
+            var phonePlusInternetRedirectButton = driver.FindElement(By.LinkText("Telefon + İnternet"));
+            var tvPlusInternetRedirectButton = driver.FindElement(By.LinkText("Tv + İnternet"));
+            var triplePackageRedirectButton = driver.FindElement(By.LinkText("3’lü Paket"));
+            var allInternetPackagesRedirectButton = driver.FindElement(By.LinkText("Tüm İnternet Paketlerini Gör"));
 
             Assert.IsNotNull(internetTab);
             Assert.IsNotNull(leanInternetRedirectButton);
@@ -113,35 +131,132 @@ namespace StoreFront.Tests
         [TestMethod]
         public void Can_Calculate_Personal_Finance_Credit()
         {
-            chromeDriver.Navigate().GoToUrl(HomePageUrl);
-            var creditTab = chromeDriver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[0];
+            driver.Navigate().GoToUrl(HomePageUrl);
+            var creditTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[0];
             creditTab.Click();
 
-            var personalFinanceCreditLabel = chromeDriver.FindElement(By.XPath("//label[@for='ihtiyacRadio']"));
+            var personalFinanceCreditLabel = driver.FindElement(By.XPath("//label[@for='ihtiyacRadio']"));
             personalFinanceCreditLabel.Click();
 
-            var amountTextBox = chromeDriver.FindElement(By.Id("Tutar"));
+            var amountTextBox = driver.FindElement(By.Id("Tutar"));
             amountTextBox.Click();
             amountTextBox.SendKeys("10000");
 
-            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)chromeDriver;
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
             javaScriptExecutor.ExecuteScript(@"$('.hk-select.form-control').attr('class', 'hk-select form-control hk-active')");
-            var expirationBox = chromeDriver.FindElement(By.XPath("//li[@data-value='12']"));
+            var expirationBox = driver.FindElement(By.XPath("//li[@data-value='12']"));
             expirationBox.Click();
             
-            var showBidsButton = chromeDriver.FindElement(By.Id("teklif-goster"));
+            var showBidsButton = driver.FindElement(By.Id("teklif-goster"));
             showBidsButton.Click();
 
-            var navigatedPageHeaderTitle = chromeDriver.FindElement(By.Id("divHeaderTitle"));
+            var navigatedPageHeaderTitle = driver.FindElement(By.Id("divHeaderTitle"));
 
-            Assert.IsNotNull(navigatedPageHeaderTitle);
+            Assert.IsTrue(navigatedPageHeaderTitle.Displayed);
+        }
+
+        [TestMethod]
+        public void Can_Calculate_Mortgage_Loan()
+        {
+            driver.Navigate().GoToUrl(HomePageUrl);
+            var creditTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[0];
+            creditTab.Click();
+
+            var housingCreditLabel = driver.FindElement(By.XPath("//label[@for='konutRadio']"));
+            housingCreditLabel.Click();
+
+            var amountTextBox = driver.FindElement(By.Id("Tutar"));
+            amountTextBox.Click();
+            amountTextBox.SendKeys("100000");
+
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+            javaScriptExecutor.ExecuteScript(@"$('.hk-select.form-control').attr('class', 'hk-select form-control hk-active')");
+            var expirationBox = driver.FindElement(By.XPath("//li[@data-value='60']"));
+            expirationBox.Click();
+
+            var showBidsButton = driver.FindElement(By.Id("teklif-goster"));
+            showBidsButton.Click();
+
+            driver.SwitchTo().ActiveElement();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var modalElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("redirect")));
+            modalElement.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
+            driver.SwitchTo().ActiveElement();
+            var navigatedPageHeaderTitle = driver.FindElement(By.Id("housingHeader"));
+
+            Assert.IsTrue(navigatedPageHeaderTitle.Displayed);
+        }
+
+        [TestMethod]
+        public void Can_Calculate_Transport_Credit()
+        {
+            driver.Navigate().GoToUrl(HomePageUrl);
+            var creditTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[0];
+            creditTab.Click();
+
+            var housingCreditLabel = driver.FindElement(By.XPath("//label[@for='tasitRadio']"));
+            housingCreditLabel.Click();
+
+            var amountTextBox = driver.FindElement(By.Id("Tutar"));
+            amountTextBox.Click();
+            amountTextBox.SendKeys("50000");
+
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+            javaScriptExecutor.ExecuteScript(@"$('.hk-select.form-control').attr('class', 'hk-select form-control hk-active')");
+            var expirationBox = driver.FindElement(By.XPath("//li[@data-value='24']"));
+            expirationBox.Click();
+
+            var showBidsButton = driver.FindElement(By.Id("teklif-goster"));
+            showBidsButton.Click();
+
+            driver.SwitchTo().ActiveElement();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var modalElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("redirect")));
+            modalElement.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
+            driver.SwitchTo().ActiveElement();
+            var navigatedPageHeaderTitle = driver.FindElement(By.Id("divHeaderTitle"));
+
+            Assert.IsTrue(navigatedPageHeaderTitle.Displayed);
+        }
+
+        [TestMethod]
+        public void Can_Calculate_SME_Loan()
+        {
+            driver.Navigate().GoToUrl(HomePageUrl);
+            var creditTab = driver.FindElement(By.ClassName("tab-selectors")).FindElements(By.TagName("li"))[0];
+            creditTab.Click();
+
+            var housingCreditLabel = driver.FindElement(By.XPath("//label[@for='kobiRadio']"));
+            housingCreditLabel.Click();
+
+            var amountTextBox = driver.FindElement(By.Id("Tutar"));
+            amountTextBox.Click();
+            amountTextBox.SendKeys("250000");
+
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+            javaScriptExecutor.ExecuteScript(@"$('.hk-select.form-control').attr('class', 'hk-select form-control hk-active')");
+            var expirationBox = driver.FindElement(By.XPath("//li[@data-value='60']"));
+            expirationBox.Click();
+
+            var showBidsButton = driver.FindElement(By.Id("teklif-goster"));
+            showBidsButton.Click();
+            
+            var navigatedPageHeaderTitle = driver.FindElement(By.Id("divHeaderTitle"));
+            
+            Assert.IsTrue(navigatedPageHeaderTitle.Displayed);
         }
 
         [TestCleanup]
         public void Cleanup()
         {
-            chromeDriver.Close();
-            chromeDriver.Dispose();
+            driver.Close();
+            driver.Dispose();
         }
     }
 }
